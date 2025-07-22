c6f53bf7de6bf8720f43aa07a0f3334c132401bd
vietgiang111001 <vietgiang111001@gmail.com>
2025-07-22 10:39:03 +0700

    Fix

--- Js/index.js
+++ Js/index.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const clientId = "79c312b7-adad-43a8-b457-22d47c4f1fb4";
     const tenantId = "b64e7779-a39b-4812-b490-1d710747aac2";
     // Tự động lấy domain hiện tại để tránh lỗi redirect nếu KO dùng github pages bỏ /PortalUI/
-    const redirectUri = window.location.origin + "/PortalUI//Pages/callback.html";
+    //const redirectUri = window.location.origin + "/Pages/callback.html";
+    const redirectUri = "https://nguyenvietgiang.github.io/PortalUI/Pages/callback.html"; 
     const scope = "openid profile email";
 
     const authUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&response_mode=fragment`;
